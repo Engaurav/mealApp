99e3c75ea808c846e1bf33d398aecea91a0a0214
Gaurav Singh <gaurav.singh323@gmail.com>
2022-05-29 01:57:42 +0530

    Added Favourite List Page and Funcunality

--- assets/javascripts/home.js
+++ assets/javascripts/home.js
@@ -1,73 +1,153 @@
 // Javascripyt to get Data from Json
 // console.log("Hello");
-fetch('../assets/data/data.json')
-.then(response => {
-   return response.json();
-})
-.then(data =>{    //
-    breakfastList(data.breakfast);  //calling breakFast list funtion to render list breakfast item
-    lunchList(data.lunch);  //calling lunch list funtion to render list lunch item
-});
-
+fetch("../assets/data/data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    //
+    breakfastList(data.breakfast); //calling breakFast list funtion to render list breakfast item
+    lunchList(data.lunch); //calling lunch list funtion to render list lunch item
+  }, true);
 
 //javascript for breakfast meal : START
 function breakfastList(breakfastData) {
-    var breakfastMeal = document.getElementById("breakfast");
+  var breakfastMeal = document.getElementById("breakfast");
+  let breakfastFavList = window.localStorage.getItem("breakfast");
+  if (breakfastFavList) {
+    breakfastFavList = JSON.parse(breakfastFavList);
+  } else {
+    breakfastFavList = [];
+  }
+  let meall = "breakfast";
 
-    breakfastData.map((data,index) => {
-        let mealItem = "<div class='breakfast-item-container'><a href='/html/singleItem.html?meal=breakfast&index="+index+"' class='meal-item-link'><img class='meal-item-img' src="+ data.img +" /><div class='meal-item-name'>"+ data.name +"</div><p class='meal-item-detail'>"+ data.detail +"</p></a></div>";
-        breakfastMeal.innerHTML += mealItem;
-    });
+  breakfastData.map((data, index) => {
+    let mealItem = ` <div class="breakfast-item-container">
+        <a href="./html/singleItem.html?meal=breakfast&index=${index}" class="meal-item-link">
+            <img class="meal-item-img" src="${data.img}" />
+            <div class="meal-item-name">${data.name}</div>
+            <p class="meal-item-detail">${data.detail}</p>
+        </a>
+        <div id="fav-unfav">
+            ${
+              breakfastFavList.includes(index)
+                ? "<button class='unfav' onclick='makeUnfavourite(" +
+                  JSON.stringify(meall) +
+                  "," +
+                  index +
+                  ")'>Unfavourite</button>"
+                : "<button class='fav' onclick='makeFavourite(" +
+                  JSON.stringify(meall) +
+                  "," +
+                  index +
+                  ")'>Favourite</button>"
+            }
+            <!--<button class="fav" onclick="makeFavourite('breakfast',${index})">Favourite</button> -->
+            <!-- <button class="unfav">Unfavourite</button> -->
+        </div>
+    </div>`;
+    breakfastMeal.innerHTML += mealItem;
+  });
 }
 
 // function for next food item of breakfast
-function breakfastNext() {    
-    var breakfastMeal = document.getElementById("breakfast");
-    var breakItem = document.getElementsByClassName("breakfast-item-container");
+function breakfastNext() {
+  var breakfastMeal = document.getElementById("breakfast");
+  var breakItem = document.getElementsByClassName("breakfast-item-container");
 
-    breakfastMeal.append(breakItem[0]);
+  breakfastMeal.append(breakItem[0]);
 }
-
 
 // function for previous food item of breakfast
 function breakfastPrevious() {
-    
-    var breakfastMeal = document.getElementById("breakfast");
-    var breakItem = document.getElementsByClassName("breakfast-item-container");
-    breakfastMeal.prepend(breakItem[breakItem.length - 1]);
+  var breakfastMeal = document.getElementById("breakfast");
+  var breakItem = document.getElementsByClassName("breakfast-item-container");
+  breakfastMeal.prepend(breakItem[breakItem.length - 1]);
 }
 
 //javascript for breakfast meal : end
 
-
-
-
 //javascript for lunch meal : START
 function lunchList(lunchData) {
-    var lunchMeal = document.getElementById("lunch");
-
-    lunchData.map((data,index) => {
-        let mealItem = "<div class='lunch-item-container'><a href='/html/singleItem.html?meal=lunch&index="+index+"' class='meal-item-link'><img class='meal-item-img' src="+ data.img +" /><div class='meal-item-name'>"+ data.name +"</div><p class='meal-item-detail'>"+ data.detail +"</p></a></div>";
-        lunchMeal.innerHTML += mealItem;
-    });
+  var lunchMeal = document.getElementById("lunch");
+  let lunchfastFavList = window.localStorage.getItem("lunch");
+  if (lunchfastFavList) {
+    lunchfastFavList = JSON.parse(lunchfastFavList);
+  } else {
+    lunchfastFavList = [];
+  }
+  let meall = "lunch";
+  lunchData.map((data, index) => {
+    let mealItem = ` <div class="lunch-item-container">
+    <a href="./html/singleItem.html?meal=lunch&index=${index}" class="meal-item-link">
+        <img class="meal-item-img" src="${data.img}" />
+        <div class="meal-item-name">${data.name}</div>
+        <p class="meal-item-detail">${data.detail}</p>
+    </a>
+    <div id="fav-unfav">
+        ${
+          lunchfastFavList.includes(index)
+            ? "<button class='unfav' onclick='makeUnfavourite(" +
+              JSON.stringify(meall) +
+              "," +
+              index +
+              ")'>Unfavourite</button>"
+            : "<button class='fav' onclick='makeFavourite(" +
+              JSON.stringify(meall) +
+              "," +
+              index +
+              ")'>Favourite</button>"
+        }
+        <!--<button class="fav" onclick="makeFavourite('lunch',${index})">Favourite</button> -->
+        <!-- <button class="unfav">Unfavourite</button> -->
+    </div>
+</div>`;
+    lunchMeal.innerHTML += mealItem;
+  });
 }
-
 
 // function for next food item of lunch
-function lunchNext() {    
-    var lunchMeal = document.getElementById("lunch");
-    var breakItem = document.getElementsByClassName("lunch-item-container");
+function lunchNext() {
+  var lunchMeal = document.getElementById("lunch");
+  var breakItem = document.getElementsByClassName("lunch-item-container");
 
-    lunchMeal.append(breakItem[0]);
+  lunchMeal.append(breakItem[0]);
 }
-
 
 // function for previous food item of lunch
 function lunchPrevious() {
-    
-    var lunchMeal = document.getElementById("lunch");
-    var breakItem = document.getElementsByClassName("lunch-item-container");
-    lunchMeal.prepend(breakItem[breakItem.length - 1]);
+  var lunchMeal = document.getElementById("lunch");
+  var breakItem = document.getElementsByClassName("lunch-item-container");
+  lunchMeal.prepend(breakItem[breakItem.length - 1]);
 }
 
 //javascript for lunch meal : End
+
+// JavaScript to Handle Favourite unfav of Meal : Start
+function makeFavourite(meal, index) {
+  //   console.log("meal", meal);
+  if (window.localStorage.getItem(meal)) {
+    let mealFavList = JSON.parse(window.localStorage.getItem(meal));
+    mealFavList.push(index);
+    window.localStorage.setItem(meal, JSON.stringify(mealFavList));
+  } else {
+    let mealFavList = [];
+    mealFavList.push(index);
+    window.localStorage.setItem(meal, JSON.stringify(mealFavList));
+  }
+  window.location.reload();
+}
+
+function makeUnfavourite(meal, index) {
+  //   console.log("meal", meal);
+  if (window.localStorage.getItem(meal)) {
+    let mealFavList = JSON.parse(window.localStorage.getItem(meal));
+    mealFavList = mealFavList.filter((value, ind) => {
+      return value !== index;
+    });
+    window.localStorage.setItem(meal, JSON.stringify(mealFavList));
+  }
+  window.location.reload();
+}
+
+// JavaScript to Handle Favourite unfav of Meal : Emd
